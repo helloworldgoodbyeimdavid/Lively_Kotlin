36721eb1feb88e8bd1fb37c02fc08f8f4089c126
helloworldgoodbyeimdavid <98780076+helloworldgoodbyeimdavid@users.noreply.github.com>
2023-11-04 20:26:22 -0700

    Update
    just practicing

---
+++ math.kt
@@ -0,0 +1,13 @@
+//this is my favorite number 420
+fun math(a: Int, b: Int, c: Int): Int {
+    return a+b+c
+}
+//this is just some random question I came up with
+//called I don't know or care call it boredom 😂
+fun math2(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int, h: Int) = a*(h+(b)*g)+(f/e)*d
+//calling every thing to display on screen and calling
+//the math( ) and math2( ) functions
+fun main( ) {
+println(math(200,120,100))
+println(math2(200,120,100,240,400,420,22,1))
+}
